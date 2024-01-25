ccf90bee34a9208ffbeba48556326e6f824844a0
Raj Kumar Maharja <rajkumarmaharjan006@gmail.com>
2024-01-25 17:16:06 +0545

    correction in objectStore error

--- JS/app.js
+++ JS/app.js
@@ -4,77 +4,69 @@ const edit = document.querySelector(".update");
 const tbody = document.querySelector("table>tbody");
 
 submitBtn.addEventListener('click', () => {
-let isEmptyField = false;
+  let isEmptyField = false;
 
-for(let i = 0; i < form.length; i++){
+  for(let i=0; i < form.length; i++){
     if(form[i].value.trim() === ""){
-        alert("Please fill in all fields before submitting.");
-        return;
-    }else{
-        isEmptyField = true;
-            break;
+      alert("please fill in all fields before submitting.");
+      return;
+    }else {
+      isEmptyField = true;
+      break;
     }
-}
+  }
 
-let idb = indexedDB.open('curd');
-
-idb.onupgradeneeded = () => {
-    let result = idb.result;
-    result.createObjectStore('data', {autoIncrement: true});
-}
-idb.onsuccess = () => {
-    let result = idb.result
-    let trans = result.transaction('data', 'readwrite');
-    let store = trans.objectStore('data');
-
-    trans.oncomplete = () => {
+  let idb = indexedDB.open('CURD', 1);
+  idb.onupgradeneeded = () => {
+      let result = idb.result;
+      result.createObjectStore('data', {autoIncrement: true})
+  }
+  idb.onsuccess = () => {
+      let result = idb.result;
+      let trans = result.transaction('data', 'readwrite');
+      let store = trans.objectStore('data');
+      store.put({
+          name: form[0].value,
+          email: form[1].value,
+          phone: form[2].value,
+          address: form[3].value,
+      })
+      trans.oncomplete = () => {
         alert("Data has been added");
         location.reload();
-    };
-
-    store.put({
-        name: form[0].value,
-        email: form[1].value,
-        phone: form[2].value,
-        address: form[3].value
-    })
-}
-
-idb.onerror = (event) => {
-    // Handle errors
-    console.error("Error opening IndexedDB", event);
-};
-
+      }
+  }
 })
 
 function read() {
-    let idb = indexedDB.open('curd');
-    idb.onsuccess = () => {
+  let idb = indexedDB.open("CURD", 1);
+
+  idb.onsuccess = () => {
       let result = idb.result;
-      let trans = result.transaction("data", "readonly");
-      let store = trans.objectStore("data");
+      let trans = result.transaction('data', 'readonly');
+      let store = trans.objectStore('data');
       let cursor = store.openCursor();
+
       cursor.onsuccess = () => {
-        let cursorResult = cursor.result;
-        if (cursorResult) {
-          console.log(cursorResult.value);
-          tbody.innerHTML += `<tr>
+          let cursorResult = cursor.result;
+          if(cursorResult){
+              tbody.innerHTML += `<tr>
               <td>${cursorResult.value.name}</td>
               <td>${cursorResult.value.email}</td>
               <td>${cursorResult.value.phone}</td>
               <td>${cursorResult.value.address}</td>
               <td onclick="update(${cursorResult.key})" class="up">Update</td>
               <td onclick="del(${cursorResult.key})" class="de">Delete</td>
-           
               </tr>`;
           cursorResult.continue();
-        }
-      };
-    };
+          }
+      }
   }
 
+}
+
   function del(e) {
-    let idb = indexedDB.open("curd");
+    let idb = indexedDB.open("CURD");
     idb.onsuccess = () => {
       let result = idb.result;
       let trans = result.transaction("data", "readwrite");
@@ -94,7 +86,7 @@ function update(e) {
 }
 
 edit.addEventListener("click", () => {
-  let idb = indexedDB.open("curd");
+  let idb = indexedDB.open("CURD");
   idb.onsuccess = () => {
     let result = idb.result;
     let trans = result.transaction("data", "readwrite");
